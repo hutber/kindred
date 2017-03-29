0a433b128b4538e4dafca6016eff28ef36ece1e1
Jamie Hutber - Home <jamie@hutber.com>
2017-03-29 23:54:17 +0100

    feat: Most basic routes and history added to app

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux'
+import createHistory from 'history/createBrowserHistory' //history api
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import nprogress from 'nprogress';
+const history = createHistory(); //Use a history moudle
+import configureStore from './store/configureStore';
+import Routes from './routes';
 
-import { AppContainer } from 'react-hot-loader';
-// AppContainer is a necessary wrapper component for HMR
+//Remove on screen tap delay
+injectTapEventPlugin();
 
-import App from './components/App';
+//Add progress bar
+nprogress.configure({ minimum: 0.15, showSpinner: false, speed: 500 });
 
-const render = (Component) => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component/>
-    </AppContainer>,
-    document.getElementById('root')
-  );
-};
 
-render(App);
+// Now you can dispatch navigation actions from anywhere!
+// store.dispatch(push('/foo'))
 
-// Hot Module Replacement API
-if (module.hot) {
-  module.hot.accept('./components/App', () => {
-    render(App)
-  });
-}
+const store = configureStore(history);
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Routes/>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('app')
+);
