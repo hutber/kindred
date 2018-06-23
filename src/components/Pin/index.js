1f2d0875ff746ea203903ec347c817e8ffd25322
Jamie Hutber <jamie@hutber.com>
2018-06-24 00:16:13 +0100

    fix: Pin page - Displays incorrectly on smaller devices
    Issues: WMVT-23021

--- src/components/Pin/index.js
+++ src/components/Pin/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import { bindActionCreators } from 'redux'
-import {Redirect} from 'react-router'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Redirect } from 'react-router';
 
 //Auth
-import RequireLogin from '../shared/auth/RequireLogin'
-import * as pinAction from '../../actions/user/pinActions'
+import RequireLogin from '../shared/auth/RequireLogin';
+import * as pinAction from '../../actions/user/pinActions';
 
 //Components
 import Logo from '../shared/logo';
@@ -18,53 +18,54 @@ import main from '../shared/main.css';
 import style from './style.css';
 
 class Pin extends React.Component {
-  constructor (props){
+  constructor(props) {
     super(props);
 
     this.state = {};
   }
 
-  componentWillMount(){
-  	this.props.deleteDigit();
+  componentWillMount() {
+    this.props.deleteDigit();
   }
 
-  render(){
-	  if(this.props.pin.pinFilledIn)
-	  	return (<Redirect push to="/pinconfirm" /> );
+  render() {
+    if (this.props.pin.pinFilledIn) return <Redirect push to="/pinconfirm" />;
 
-	  return (
+    return (
       <div className={main.middle}>
-	      <div className={main.container}>
-		      <RequireLogin />
-		      <Logo />
-		      <h1>Security check</h1>
-		      <p className={style.p}>We'll keep all your data secure and anonymous. Please chose a 4 digit pin code for extra protection</p>
-		      <div className={style.pinContainer}>
-			      {this.props.pin.pin.map((item, index) => <div key={`pinBox${index}`} className={`${style.pinItem} ${item!==null ? style.full : ''}`}></div>)}
-		      </div>
-			      <div className={style.digitContainer}>
-			      {[1,2,3,4,5,6,7,8,9].map((val) => <PinDigit digit={val} key={`pin${val}`} /> )}
-			      <PinDigit digit="forgotten" />
-			      <PinDigit digit="0" />
-			      <PinDigit digit="clear"/>
-			      </div>
-	      </div>
+        <div className={main.container}>
+          <RequireLogin />
+          <Logo />
+          <h1>Security check</h1>
+          <p className={style.p}>We'll keep all your data secure and anonymous. Please chose a 4 digit pin code for extra protection</p>
+          <div className={style.pinContainer}>
+            {this.props.pin.pin.map((item, index) => (
+              <div key={`pinBox${index}`} className={`${style.pinItem} ${item !== null ? style.full : ''}`} />
+            ))}
+          </div>
+          <div className={style.digitContainer}>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(val => <PinDigit digit={val} key={`pin${val}`} />)}
+            <PinDigit digit="forgotten" />
+            <PinDigit digit="0" />
+            <PinDigit digit="clear" />
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-function matchStateToProps(state){
+function matchStateToProps(state) {
   return {
-  	pin: state.user.pin,
+    pin: state.user.pin,
     dates: state.dates.dates
-  }
+  };
 }
 
-function matchDispatchToProps(dispatch){
+function matchDispatchToProps(dispatch) {
   return {
-	  deleteDigit : bindActionCreators(pinAction.deleteDigit, dispatch)
-  }
+    deleteDigit: bindActionCreators(pinAction.deleteDigit, dispatch)
+  };
 }
 
-export default connect(matchStateToProps,matchDispatchToProps)(Pin);
+export default connect(matchStateToProps, matchDispatchToProps)(Pin);
